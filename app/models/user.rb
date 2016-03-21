260226da60a2c192f199af9bdf2f592193e9d18b
MaggieMoss <maggiebmoss@gmail.com>
2016-03-20 22:59:32 -0400

    turned it into a sinatra project

--- app/models/user.rb
+++
@@ -1,6 +0,0 @@
-# We want our models to inherit from active record base so our model inherits
-# all of the active record methods (.find(), .where())
-
-class User < ActiveRecord::Base
-	has_many :contacts
-end
